c98813f8bbfbfb8964989c15f9048a60b714349e
alejandromero <alejandroromero.umsnh@gmail.com>
2016-11-24 09:56:52 -0600

    Delete Ejercicio1.py

--- Laboratorio 1/Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-#Laboratorio 1
-#Alejandro Romero Amezcua
-#Ejercicio 1
-sumanumeros=0
-a=int(input("Hasta que numero desea que se efectue la suma "))
-for i in range(0,a):
-    sumanumeros=sumanumeros+i
-print sumanumeros
