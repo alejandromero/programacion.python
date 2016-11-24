4907b88555ba6cfea315d18e5af9db3ec64f3c64
alejandromero <alejandroromero.umsnh@gmail.com>
2016-11-24 09:37:49 -0600

    Add files via upload

---
+++ Laboratorio 1/Ejercicio1.py
@@ -0,0 +1,8 @@
+#Laboratorio 1
+#Alejandro Romero Amezcua
+#Ejercicio 1
+sumanumeros=0
+a=int(input("Hasta que numero desea que se efectue la suma "))
+for i in range(0,a):
+    sumanumeros=sumanumeros+i
+print sumanumeros
---
+++ Laboratorio 1/Ejercicio3.py
@@ -0,0 +1,15 @@
+#Laboratorio 1
+#Alejandro Romero Amezcua
+#Ejercicio3
+areacirculo=0
+volumenesfera=0
+pi=3.14159265359
+r=int(input("Introdusca el radio del circulo/esfera: "))
+def area(a):
+    areacirculo=pi*a*a
+    return areacirculo
+def volumen(a):
+    volumenesfera=(4/3)*pi*a*a*a
+    return volumenesfera
+print "El area del circulo es: ",area(r)
+print "El volumen de la esfera es:",volumen(r)
