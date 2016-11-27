ef4b9260e2bf9723182c4c717cc9b4d2b103d957
alejandromero <alejandroromero.umsnh@gmail.com>
2016-11-26 22:33:57 -0600

    Tarea 2

--- Laboratorio 2/ejercicio3.py
+++ Laboratorio 2/ejercicio3.py
@@ -28,6 +28,7 @@ def verticesc23(a,x,y,z): #Obtiene los vertices faltantes del triangulo
 			y.append((a*math.sin((0*math.pi)/180))+x[i])
 			z.append(a*math.sin((120*math.pi)/180)+y[i])
 	return x,y,z
+
 def construirt(a,x,y,z): #Construye el triangulo con los vertices dados
 	global triangulo
 	verticesc23(a,x,y,z)
@@ -39,6 +40,7 @@ def p0r(p,x,y,z): #Elige un punto al azar
 	p[0]=(np.random.uniform(x[0],y[0]))
 	p[1]=(np.random.uniform(x[1],z[1]))
 	return p
+
 def contienep(p,f): #Verifica que el punto este dentro del triangulo
 		if f.contains_point(p)==True:
 			return 1
@@ -53,13 +55,16 @@ def azarv(x,y,z): #Seleccionar vertice al azar
 	return verticeazar
 def puntom(p,v,p1,tpx,tpy):
 	p1=[]
-	for i in range(0,len(p)):
+	for i in range(0,len(v)):
 		if i==0:
 			p1.append((p[i]+v[i])/2)
 		else:
 			p1.append((p[i]+v[i])/2)
+	p[0]=p1[0]
+	p[1]=p1[1]
 	tpx.append(p1[0])
 	tpy.append(p1[1])
+	return p
 	return p1
 	return tp
 	
@@ -70,14 +75,15 @@ def imprimir(t,tpx,tpy): #Imprime el triangulo y los puntos en la pantalla
 	plt.axis('scaled')
 	plt.show()
 
+j=0
+while j==0: #Asegurar que p0(inicial) este dentro del triangulo
+	p0r(p0,vertice1,vertice2,vertice3)
+	if contienep(p0,triangulo)==1:
+		j=1
+	else:
+		pass
+
 for i in range(0,repeticiones):
-	j=0
-	while j==0:
-		p0r(p0,vertice1,vertice2,vertice3)
-		if contienep(p0,triangulo)==1:
-			j=1
-		else:
-			pass
 	puntom(p0,azarv(vertice1,vertice2,vertice3),p1,totalpuntosx,totalpuntosy)
 #print "\n",totalpuntosx,"\n",totalpuntosy	
 imprimir(triangulo,totalpuntosx,totalpuntosy)
