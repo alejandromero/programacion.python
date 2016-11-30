11c7faa23b515cf42438fd2466b20bcc14e2aec9
alejandromero <alejandroromero.umsnh@gmail.com>
2016-11-30 00:07:20 -0600

    Tarea 3

--- Laboratorio 3/Ejercicio7.py
+++ Laboratorio 3/Ejercicio7.py
@@ -9,6 +9,7 @@ def nombresc(a): #Si el nombre comienza con consonante
 	for i in range(0,len(a)-1):
 		b=b+a[i+1]
 	return b
+
 def evaluarc(a):
 	j=0
 	while j==0:
@@ -18,11 +19,24 @@ def evaluarc(a):
 		else:
 			a=nombresc(a)
 	return a
+
+def verso(a):
+	print a,a
+	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+		print "bob"+a
+	else:
+		print "bob"+evaluarc(a)
+	print "banana bana"
+	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+		print "fof"+a
+	else:
+		print "fof"+evaluarc(a)
+	print "fi fai mo"
+	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+		print "m"+a
+	else:
+		print "m"+evaluarc(a)
+	print a
 	
 nombre=raw_input("Ingrese un nombre corto: ")
-nombre2=evaluarc(nombre)
-
-def verso(a,b):
-	print a," ",a
-	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
-		print "bob"
+verso(nombre)
