938bfde0d2c49faa1979b6522dd1adc92b0c2b07
alejandromero <alejandroromero.umsnh@gmail.com>
2016-11-29 23:55:14 -0600

    Tarea 3

---
+++ Laboratorio 3/Ejercicio1.py
@@ -0,0 +1,23 @@
+#Laboratorio 2
+#Alejandro Romero Amezcua
+#29 de noviembre de 20126
+
+import math
+
+lat1=float(input("Ingrese la primera latitud: "))
+lon1=float(input("Ingrese la primera longitud: "))
+lat2=float(input("Ingrese la segunda latitud: "))
+lon2=float(input("Ingrese la segunda longitud: "))
+
+def gar(x1,y1,x2,y2):
+	x1=(x1*math.pi)/180
+	y1=(y1*math.pi)/180
+	x2=(x2*math.pi)/180
+	y2=(y2*math.pi)/180
+        return x1,y1,x2,y2
+    
+def dis(x1,y1,x2,y2):
+    d=6371.01*math.acos((math.sin(gar(x1,y1,x2,y2)[0]))*(math.sin(gar(x1,y1,x2,y2)[2]))+((math.cos(gar(x1,y1,x2,y2)[0]))*(math.cos(gar(x1,y1,x2,y2)[2]))*(math.cos((gar(x1,y1,x2,y2)[1])-(gar(x1,y1,x2,y2)[3])))))
+    return d
+
+print "La distancia entre los puntos ingresados es: ", dis(lat1,lon1,lat2,lon2)," kilometros"
---
+++ Laboratorio 3/Ejercicio6.py
@@ -0,0 +1,22 @@
+#Laboratorio 3
+#Alejandro Romero Amezcua 
+#29 de noviembre de 2016
+
+print "\nPrograma que transforma la longevidad humana en perruna\n" #Una disculpa por la sustitucion de la n en donde no va, python no me deja trabajar con la otra letra ni acetos
+
+anospersona=int(raw_input("Ingrese la cantidad de anos persona: "))
+if anospersona<0:
+	anospersona=int(raw_input("Ingrese un numero positivo: "))
+else:
+	pass
+
+def conversion(a):
+	ap=0 #anos perro
+	for i in range(1,a+1):
+		if i<=2:
+			ap=ap+10.5
+		else:
+			ap=ap+4
+	return ap
+
+print conversion(anospersona)
