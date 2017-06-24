6a3b6c02559d47367fd67093fffb5f64f036b734
alejandromero <alejandroromero.umsnh@gmail.com>
2017-06-23 19:28:48 -0500

    Actualizacion

---
+++ Computacion II/hrk-8/Banco.py
@@ -0,0 +1,57 @@
+import datetime
+
+class trans():
+	def __init__(self,id,date,acnt,amount,type):
+		self.id=id
+		self.date=date
+		self.acnt=acnt
+		self.amount=amount
+		self.type=type
+	def __repr__(self):
+		return 'Transaction '+str(self.id)
+
+class trans_list():
+	def __init__(self):
+		self.transactions=[]
+		self.id=1
+	def add(self):
+		date=datetime.date(*map(int,raw_input("Date: ").split()))
+		acnt=int(raw_input("Account number: "))
+		amount=int(raw_input("Amount: "))
+		type=raw_input("Transaction type: ").lower()
+		self.transactions.append(trans(self.id,date,acnt,amount,type))
+		self.id+=1
+	def calc_deposits(self):
+		total=0
+		date1=datetime.date(*map(int,raw_input("Desde el: ").split()))
+		date2=datetime.date(*map(int,raw_input("Hasta el: ").split()))
+		for i in self.transactions:
+			if date1<i.date<date2:
+				if i.type=='deposito':
+					total+=i.amount
+				else:
+					pass
+		return total
+	def calc_year(self):
+		total_dep=0
+		total_ext=0
+		year=int(raw_input("Year: "))
+		for i in self.transactions:
+			if i.date.year==year:
+				if i.type=='deposito':
+					total_dep+=i.amount
+				elif i.type=='extraccion':
+					total_ext+=i.amount
+		
+		return total_dep,total_ext
+	def calc_account(self):
+		total=0
+		account=int(raw_input('Account number: '))
+		year=int(raw_input('Year: '))
+		for i in self.transactions:
+			if i.acnt==account:
+				if i.type=='deposito':
+					total+=i.amount
+				elif i.type=='extraccion':
+					total-=i.amount
+		return total
---
+++ Computacion II/hrk-8/Empleados.py
@@ -0,0 +1,62 @@
+class Stack():
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def push(self, item):
+		self.items.append(item)
+
+	def pop(self):
+		return self.items.pop()
+
+	def peek(self):
+		return self.items[len(self.items)-1]
+
+	def size(self):
+		return len(self.items)
+		
+class Task():
+	def __init__(self,id):
+		self.area=raw_input('Area solicitante de la tarea: ')
+		self.description=raw_input('Descipcion de la tarea: ')
+		self.id=id
+	def __repr__(self):
+		return self.description
+		
+class Employee():
+	def __init__(self,id):
+		self.id=id
+		self.tasks_number=0
+		self.tasks=Stack()
+	def add_task(self):
+		self.tasks_number+=1
+		self.tasks.push(Task(self.tasks_number))
+	def __repr__(self):
+		return 'Empleado '+str(self.id)
+
+class Employee_list():
+	def __init__(self):
+		self.em_list=[]
+		self.employee_id=1
+	def add_employee(self):
+		self.em_list.append(Employee(self.employee_id))
+		self.employee_id+=1
+	def task(self):
+		id=int(raw_input('Codigo de empleado: '))
+		for i in self.em_list:
+			if i.id==id:
+				i.add_task()
+				break
+			else:
+				pass
+	def task_min(self):
+		sorted(self.em_list,key=lambda x: x.tasks_number)[0].add_task()
+	def process_tasks(self):
+		id1=int(raw_input('Desde el id: '))
+		id2=int(raw_input('Hasta el id: '))
+		for i in self.em_list:
+			if id1<=i.id<=id2:
+				while i.tasks.isEmpty()!=True:
+					print 'Empleado ',i.id,' ',i.tasks.pop()
